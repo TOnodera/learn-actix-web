c9156194ca063179341e7b9aa39d270dbd628ed2
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-07-11 12:06:10 +0000

    スコープ

--- src/main.rs
+++ src/main.rs
@@ -14,13 +14,14 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+async fn index() -> impl Responder {
+    "Hello world"
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+        App::new().service(web::scope("/app").route("/index.html", web::get().to(index)))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
